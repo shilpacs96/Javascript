08e39156a4251f27caf3c24110486d5fe1f52301
SHILPA SURENDRAN <33292032+shilpacs96@users.noreply.github.com>
2024-03-22 08:18:10 +0000

    initial changes

---
+++ basics/variables.js
@@ -0,0 +1,11 @@
+const accountId=123;
+let custName='Hari';
+var email= 'hari@gmail.com';
+password='abc'
+let accState;
+
+// accountId=23;
+custName='akhil'
+email='akhil@gmail.com'
+console.log(accountId)
+console.table([accountId,custName,email,password,accState])
