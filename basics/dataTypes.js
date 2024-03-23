473ef09bee20dfeecf06d7339e1dc88e4e0a7c67
SHILPA SURENDRAN <33292032+shilpacs96@users.noreply.github.com>
2024-03-23 12:39:00 +0000

    datatypes

---
+++ basics/dataTypes.js
@@ -0,0 +1,18 @@
+"use strict" //using strict rule
+//primitive data types
+//number
+let a=10;
+//string
+let b="hello"
+//boolean
+let c=true
+//null-is stand alone value plus a data type
+//undefined-the value is declared but not initialized-value is not assigned
+//symbol-unique component
+//non primitive data type
+//object
+let d={1:"hello",2:"world",3:10}
+//typeof is used to identify the type of the variable
+
+console.log(typeof(a))
+console.table([typeof(b),typeof(c),typeof(d)])
